caa22d65fdbed344967d2534f540373ffbc3379c
Kousttav Biswas <kousttavb@gmail.com>
2025-09-12 19:35:25 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (83.41%) - LeetSync

--- 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.py
+++ 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.py
@@ -1,9 +1,7 @@
 class Solution:
     def makeTheIntegerZero(self, num1: int, num2: int) -> int:
-        for k in range(1, 61):
-            x = num1 - num2 * k
-            if x < k:
-                return -1
-            if k >= x.bit_count():
+        for k in range(60):
+            target = num1-k*num2
+            if target.bit_count() <= k and target >= k:
                 return k
         return -1
