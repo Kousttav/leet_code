bf921b45a339683a19f2c6cab398c6fc7ff7262f
Kousttav Biswas <kousttavb@gmail.com>
2025-10-11 12:43:50 +0530

    Time: 27 ms (47.20%) | Memory: 23.1 MB (94.06%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -5,17 +5,15 @@ class Solution:
         total=1
         for k in range(len(nums)):
             total*=nums[k]
-        print(total)
         other=1
         for i in range(len(nums)):
             if nums[i]!=0:
                 other*=nums[i]
-        print(other)
         l=[] 
         for j in range(len(nums)):
             if nums[j]!=0:
-                l.append(int(total/nums[j]))
+                l.append(total//nums[j])
             else:
-                l.append(int(other))
+                l.append(other)
         return l
         
