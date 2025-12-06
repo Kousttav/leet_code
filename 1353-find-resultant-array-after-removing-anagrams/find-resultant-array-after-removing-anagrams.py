2c5d2b04548680a37e5a5de27c4f8d9c90f64c5e
Kousttav Biswas <kousttavb@gmail.com>
2025-12-06 16:39:32 +0530

    Time: 3 ms (89.58%) | Memory: 17.9 MB (29.39%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
@@ -1,14 +1,8 @@
-from collections import Counter
 class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
-        if not words:
-            return []
-        r= [words[0]]
-        l= tuple(sorted(words[0]))
-
-        for i in range(1, len(words)):
-            c= tuple(sorted(words[i]))
-            if c != l:
-                r.append(words[i])
-                l= c
-        return r
+        l=[]
+        l.append(words[0])
+        for i in range (1,len(words)):
+            if sorted(words[i])!=sorted(l[-1]):
+                l.append(words[i])
+        return l
