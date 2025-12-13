17deb574e717dc371f5b281249313069674e5cdb
Kousttav Biswas <kousttavb@gmail.com>
2025-12-13 11:32:46 +0530

    Time: 1 ms (94.86%), Space: 18 MB (51.45%) - LeetHub

---
+++ 3606-coupon-code-validator/3606-coupon-code-validator.py
@@ -0,0 +1,23 @@
+from typing import List
+import re
+
+class Solution:
+    def validateCoupons(self, code: List[str], businessLine: List[str], isActive: List[bool]) -> List[str]:
+        total_business = ["electronics", "grocery", "pharmacy", "restaurant"]
+        storage = {b: [] for b in total_business}
+
+        for i in range(len(code)):
+            if (
+                businessLine[i] in total_business
+                and isActive[i]
+                and code[i] != ""
+                and code[i].replace("_","a").isalnum()
+            ):
+                storage[businessLine[i]].append(code[i])
+
+        output = []
+        for business in total_business:
+            storage[business].sort()
+            output.extend(storage[business])
+
+        return output
