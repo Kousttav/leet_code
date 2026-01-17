e0f4e15ef155877679a69694cda013f4f456f17a
Kousttav Biswas <kousttavb@gmail.com>
2026-01-17 16:43:47 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (5.33%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -1,23 +1,12 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         new=s.split(" ")
-        if len(pattern) != len(new):
+        w={}
+        if len(pattern)!=len(new):
             return False
-        p=[]
-        for i in pattern:
-            if i not in p:
-                p.append(i)
-                
-        if len(p)!=len(set(new)):
-            return False
-        d = {}
-        for i in range(len(pattern)):
-            if pattern[i] not in d:
-                d[pattern[i]] = new[i]
-        new_s=""
-        for i in pattern:
-            new_s=new_s+" "+d[i]
-        return new_s.strip()==s
-
-
-        
+        for i in range(len(new)):
+            if new[i] not in w:
+                w[new[i]]=pattern[i]
+            elif w[new[i]]!=pattern[i]:
+                return False
+        return len(set(w.keys()))==len(set(w.values()))
