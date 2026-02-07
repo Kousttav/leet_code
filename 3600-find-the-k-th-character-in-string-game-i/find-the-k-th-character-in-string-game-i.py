cb1029dba532da6a260d66e9e300d9ee25269c8d
Kousttav Biswas <kousttavb@gmail.com>
2026-02-07 17:00:20 +0530

    Time: 12 ms (31.98%) | Memory: 19.1 MB (37.02%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.py
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.py
@@ -1,29 +1,9 @@
 class Solution:
     def kthCharacter(self, k: int) -> str:
-        index = k - 1
-
-        # calculate the number of set bits
-        # this will be the total number of times the character has been incremented
-        increments = 0
-        
-        while index > 0:
-            # find the largest power of 2 less than or equal to the current index
-            # this represents the length of the string before the current char was generated
-            p = 1
-            while p * 2 <= index:
-                p *= 2
-
-            # since we must subtract p to find the parent index, it means our character
-            # was in the second half of the string generation
-            # so, we count an increment
-            increments += 1
-
-            # subtract the largest power of two from the index
-            index -= p
-        
-        # calculate the final character
-        # start with the ASCII value of 'a' and add the number of increments
-        # use the modulo to handle the wrap-around from 'z' to 'a'
-        final_char_code = ord('a') + (increments % 26)
-        
-        return chr(final_char_code)
+        word="a"
+        while len(word) < k:
+            temp=""
+            for i in word:
+                temp+= chr(ord(i) + 1)
+            word+=temp
+        return word[k-1]
