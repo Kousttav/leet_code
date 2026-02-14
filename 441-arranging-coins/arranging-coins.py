633faeb7d3bf251a9985dcf37363fa2757a0df48
Kousttav Biswas <kousttavb@gmail.com>
2026-02-14 17:50:06 +0530

    Time: 3 ms (66.82%) | Memory: 19.4 MB (42.14%) - LeetSync

--- 441-arranging-coins/arranging-coins.py
+++ 441-arranging-coins/arranging-coins.py
@@ -1,8 +1,17 @@
+# class Solution:
+#     def arrangeCoins(self, n: int) -> int:
+#         i=0
+#         while n>=0:
+#             i+=1
+#             n=n-i
+#         return i-1
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        i=0
-        while n>=0:
-            i+=1
-            n=n-i
-        return i-1
-        
+        left, right = 0, n
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * (mid + 1) // 2 <= n:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return right     
