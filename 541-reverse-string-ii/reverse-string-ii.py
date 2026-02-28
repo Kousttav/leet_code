b1e0c500aa1cde93eb9e0ad42c1752503f1e5ff5
Kousttav Biswas <kousttavb@gmail.com>
2026-02-28 17:07:40 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (10.82%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.py
+++ 541-reverse-string-ii/reverse-string-ii.py
@@ -1,13 +1,12 @@
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
-        ans = ""
-        for i in range(0, len(s), 2*k):
-            nw = s[i:i+k]
-            rem = s[i+k:i+2*k]
-            st = ""
-            for ch in nw:
-                st = ch + st 
-            
-            ans = ans + st + rem
-        
-        return ans
+        f=True
+        st = ""
+        for i in range(0, len(s), k):
+            if f:
+                st+=s[i:i+k][::-1]
+                f=False      
+            else:
+                st+=s[i:i+k]
+                f=True
+        return st
