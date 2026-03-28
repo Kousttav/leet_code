fff093c239e1d1faddbf8a231ed9b55dffc383e5
Kousttav Biswas <kousttavb@gmail.com>
2026-03-28 11:19:41 +0530

    Time: 17 ms (71.25%) | Memory: 21.2 MB (67.40%) - LeetSync

--- 575-distribute-candies/distribute-candies.py
+++ 575-distribute-candies/distribute-candies.py
@@ -1,7 +1,6 @@
 class Solution:
     def distributeCandies(self, candyType: List[int]) -> int:
         l=set(candyType)
-        n=len(candyType)
-        r=min(n//2,len(l))
-        return r
+        n=len(candyType)//2
+        return min(n,len(l))
                 
