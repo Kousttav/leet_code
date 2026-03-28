8172a73723b96658f3267cfdbd1137fae11df0d8
Kousttav Biswas <kousttavb@gmail.com>
2026-03-28 16:45:59 +0530

    Time: 1 ms (74.96%) | Memory: 19.2 MB (92.23%) - LeetSync

---
+++ 837-most-common-word/most-common-word.py
@@ -0,0 +1,21 @@
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        banned.append(" ")
+        nw=""
+        for wd in paragraph:
+            if wd.isalpha() or wd==" ":
+                nw+=wd
+            else:
+                nw+=" "
+        nw=nw.lower().split()
+        d={}
+        for i in nw:
+            if i not in d:
+                d[i]=1
+            else:
+                d[i]+=d[i]
+        print(d)
+        for i in set(banned):
+            if i in d.keys():
+                d.pop(i)
+        return max(d,key=d.get)
