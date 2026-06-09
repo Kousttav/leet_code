202fc080474a1745da495a3ff2dea535fff84557
Kousttav Biswas <kousttavb@gmail.com>
2026-06-09 13:53:45 +0530

    Add solution for First Bad Version

--- LeetCode/First-Bad-Version/Solution.py
+++ LeetCode/First-Bad-Version/Solution.py
@@ -5,7 +5,7 @@
 5    def firstBadVersion(self, n: int) -> int:
 6        left, right = 1, n
 7        while left < right:
-8            mid = (left + right) // 2
+8            mid = left + ( right - left ) // 2
 9            if isBadVersion(mid):
 10                right = mid
 11            else:
