601d6c35b598e9cba34e4011b19f7cd18cf59c61
Kousttav Biswas <kousttavb@gmail.com>
2026-07-05 20:06:40 +0530

    Add solution for Zigzag Conversion

--- LeetCode/Zigzag-Conversion/Solution.py
+++ LeetCode/Zigzag-Conversion/Solution.py
@@ -1,20 +1,16 @@
 1class Solution:
 2    def convert(self, s: str, numRows: int) -> str:
-3        if numRows == 1:
-4            return s
-5        
-6        rows = [""] * numRows
-7        curr_row = 0
-8        step = 1 
-9
-10        for char in s:
-11            rows[curr_row] += char
-12
-13            if curr_row == 0:
-14                step = 1
-15            elif curr_row == numRows -1:
-16                step = -1
-17            
-18            curr_row += step
-19        return "".join(rows)
-20
+3        if numRows==1: return s
+4        step=1
+5        row=[""]*numRows
+6        cur=0
+7        for c in s:
+8            row[cur]+=c
+9            if cur==0:
+10                step=1
+11            elif cur == numRows-1:
+12                step=-1
+13            cur+=step
+14        return "".join(row)
+15
+16        
