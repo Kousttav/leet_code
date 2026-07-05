3dc4059535853b4555a643e99cd9efa00d041933
Kousttav Biswas <kousttavb@gmail.com>
2026-07-05 20:07:14 +0530

    Time: 6 ms (88.41%) | Memory: 19.3 MB (79.72%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -1,6 +1,6 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        if numRows==1: return s
+        if numRows==1 or numRows >= len(s): return s
         step=1
         row=[""]*numRows
         cur=0
