1f9985850ad078d671de3a3aac65519af1504c6c
Kousttav Biswas <kousttavb@gmail.com>
2026-07-05 22:57:11 +0530

    Add solution for Count and Say

--- LeetCode/Count-and-Say/Solution.py
+++ LeetCode/Count-and-Say/Solution.py
@@ -1,25 +1,14 @@
-class Solution:
-    def RLE(self, sequence):
-        curr = sequence[0]
-        count = 0
-        ans = ""
-        
-        
-        for num in sequence:
-            if curr == num:
-                count += 1
-            else:
-                ans += f'{count}{curr}'
-                curr = num
-                count = 1
-        
-        
-        return ans + f'{count}{curr}'
-    
-    
-    def countAndSay(self, n: int) -> str:
-        if n == 1:
-            return "1"
-        
-        
-        return self.RLE(self.countAndSay(n - 1))
+1class Solution:
+2    def countAndSay(self, n: int) -> str:
+3        s = "1"
+4        for i in range(n-1):
+5            count = 1
+6            tmp = ""
+7            for j in range(len(s)):
+8                if j == len(s) - 1 or s[j] != s[j+1]:
+9                    tmp += str(count) + s[j]
+10                    count = 1
+11                else:
+12                    count += 1
+13            s = tmp
+14        return s
