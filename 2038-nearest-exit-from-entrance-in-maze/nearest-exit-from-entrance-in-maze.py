18c6c0324e073e76db754d74d847d71bc4e7d385
Kousttav Biswas <kousttavb@gmail.com>
2026-07-13 20:07:03 +0530

    Time: 59 ms (68.45%) | Memory: 20.8 MB (74.99%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.py
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.py
@@ -1,27 +1,40 @@
-from queue import Queue
+from collections import deque
+from typing import List
+
 class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
-        r=len(maze)
-        c=len(maze[0])
-        directions=[(0,1),(0,-1),(1,0),(-1,0)]
-        q=Queue()
-        q.put((entrance[0],entrance[1]))
-        maze[entrance[0]][entrance[1]]="+"
-        s=0
-        while not q.empty():
-            size=q.qsize()
-            for _ in range(size):
-                x,y=q.get()
-                if (x, y) != tuple(entrance) and (
-                    x == 0 or x == r-1 or y == 0 or y == c-1
-                ):
-                    return s
-                for dx,dy in directions:
-                    nx=x+dx
-                    ny=y+dy
-                    if 0 <= nx < r and 0 <= ny < c and maze[nx][ny] != "+":
-                        q.put((nx,ny))
-                        maze[nx][ny]="+"
-            s+=1
-        return -1
+        m, n = len(maze), len(maze[0])
 
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+        q = deque()
+        q.append((entrance[0], entrance[1]))
+
+        maze[entrance[0]][entrance[1]] = '+'   # Mark entrance as visited
+        steps = 0
+
+        while q:
+            size = len(q)
+
+            for _ in range(size):
+                x, y = q.popleft()
+
+                if (x, y) != (entrance[0], entrance[1]) and (
+                    x == 0 or x == m - 1 or y == 0 or y == n - 1
+                ):
+                    return steps
+
+                for dx, dy in directions:
+                    nx, ny = x + dx, y + dy
+
+                    if (
+                        0 <= nx < m
+                        and 0 <= ny < n
+                        and maze[nx][ny] != '+'
+                    ):
+                        q.append((nx, ny))
+                        maze[nx][ny] = '+'   # Mark as visited
+
+            steps += 1
+
+        return -1
