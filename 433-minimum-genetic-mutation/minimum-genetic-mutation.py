13fc330a12e16a11d36dd67469a6e2459ee523d5
Kousttav Biswas <kousttavb@gmail.com>
2026-07-13 11:36:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (10.45%) - LeetSync

--- 433-minimum-genetic-mutation/minimum-genetic-mutation.py
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.py
@@ -2,24 +2,22 @@ from queue import Queue
 class Solution:
     def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
         bank=set(bank)
-        visited=set()
         if endGene not in bank:
             return -1
+        visited=set()
         q=Queue()
         q.put((startGene,0))
         g='ACGT'
         while not q.empty():
-            gene, steps = q.get()
-            if gene == endGene:
+            gene,steps=q.get()
+            if gene==endGene:
                 return steps
-
             for i in range(len(startGene)):
                 for ch in g:
-                    if ch == gene[i]:
+                    if ch==gene[i]:
                         continue
-                    temp = gene[:i] + ch + gene[i+1:]
-                    if temp in bank and temp not in visited:
+                    temp = gene[:i]+ch+gene[i+1:]
+                    if temp not in visited and temp in bank:
                         visited.add(temp)
                         q.put((temp,steps+1))
         return -1
-        
