5d3a9cecd3f1ffe9aa3de46f36ffa34be0821160
Kousttav Biswas <kousttavb@gmail.com>
2026-07-13 11:36:29 +0530

    Add solution for Minimum Genetic Mutation

--- LeetCode/Minimum-Genetic-Mutation/Solution.py
+++ LeetCode/Minimum-Genetic-Mutation/Solution.py
@@ -2,24 +2,22 @@
 2class Solution:
 3    def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
 4        bank=set(bank)
-5        visited=set()
-6        if endGene not in bank:
-7            return -1
+5        if endGene not in bank:
+6            return -1
+7        visited=set()
 8        q=Queue()
 9        q.put((startGene,0))
 10        g='ACGT'
 11        while not q.empty():
-12            gene, steps = q.get()
-13            if gene == endGene:
+12            gene,steps=q.get()
+13            if gene==endGene:
 14                return steps
-15
-16            for i in range(len(startGene)):
-17                for ch in g:
-18                    if ch == gene[i]:
-19                        continue
-20                    temp = gene[:i] + ch + gene[i+1:]
-21                    if temp in bank and temp not in visited:
-22                        visited.add(temp)
-23                        q.put((temp,steps+1))
-24        return -1
-25        
+15            for i in range(len(startGene)):
+16                for ch in g:
+17                    if ch==gene[i]:
+18                        continue
+19                    temp = gene[:i]+ch+gene[i+1:]
+20                    if temp not in visited and temp in bank:
+21                        visited.add(temp)
+22                        q.put((temp,steps+1))
+23        return -1
