7f9d990c2b8da4f5b4a7c3a7b3a1a7d09ee4813e
Kousttav Biswas <kousttavb@gmail.com>
2026-07-14 13:47:08 +0530

    Add solution for Keys and Rooms

--- LeetCode/Keys-and-Rooms/Solution.py
+++ LeetCode/Keys-and-Rooms/Solution.py
@@ -1,14 +1,17 @@
-1from collections import defaultdict
-2class Solution:
-3    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-4        graph = defaultdict(list)
-5        for i in range(len(rooms)):
-6            graph[i] = rooms[i]
-7        def dfs(rooms,source, visited): 
-8            visited[source]=True 
-9            for i in graph[source]: 
-10                if not visited[i]: 
-11                     dfs(rooms,i, visited)
-12        visited = [False]*len(rooms)
-13        dfs(rooms,0, visited)
-14        return False not in visited
+1class Solution:
+2    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+3        n = len(rooms)
+4        visited = [False] * n
+5        visited[0] = True
+6        stack = [0]
+7        count = 1  # Number of rooms visited so far
+8
+9        while stack:
+10            room = stack.pop()
+11            for key in rooms[room]:
+12                if not visited[key]:
+13                    visited[key] = True
+14                    count += 1
+15                    stack.append(key)
+16
+17        return count == n
