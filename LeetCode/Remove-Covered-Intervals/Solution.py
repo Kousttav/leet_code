3d3e10b9ab94b904f8b68d6143270fa8807e3901
Kousttav Biswas <kousttavb@gmail.com>
2026-07-14 21:45:18 +0530

    Add solution for Remove Covered Intervals

--- LeetCode/Remove-Covered-Intervals/Solution.py
+++ LeetCode/Remove-Covered-Intervals/Solution.py
@@ -1,13 +1,13 @@
 1class Solution:
 2    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
-3        covered = [False] * len(intervals)
-4
-5        for i in range(len(intervals)):
-6            for j in range(len(intervals)):
-7                if i != j:
-8                    if (intervals[j][0] <= intervals[i][0] and
-9                        intervals[i][1] <= intervals[j][1]):
-10                        covered[i] = True
-11                        break
-12
-13        return covered.count(False)
+3        intervals.sort(key = lambda x:x[0])
+4        count = len(intervals)
+5        prev_l = intervals[0][0]
+6        prev_r = intervals[0][1]
+7        for i in range(1, len(intervals)):
+8            # 1, 4,  2, 3
+9            if intervals[i][1] <= prev_r or intervals[i][0] == prev_l:
+10                count -= 1
+11            prev_l = intervals[i][0]
+12            prev_r = max(prev_r, intervals[i][1])
+13        return count
