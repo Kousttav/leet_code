f8ae77a730002b38e89deac08173fd13879afb5b
Kousttav Biswas <kousttavb@gmail.com>
2026-07-14 19:36:16 +0530

    Add solution for Rank Transform of an Array

--- LeetCode/Rank-Transform-of-an-Array/Solution.py
+++ LeetCode/Rank-Transform-of-an-Array/Solution.py
@@ -1,10 +1,13 @@
 1class Solution:
 2    def arrayRankTransform(self, arr: List[int]) -> List[int]:
-3        sorted_unique = sorted(set(arr)) 
-4        rank_map = {}
-5        index = 1
-6        for num in sorted_unique: 
-7            rank_map[num] = index
-8            index += 1
-9        return [rank_map[num] for num in arr]
-10
+3        l = sorted(set(arr)) 
+4        rank = {}
+5        i = 1
+6        for n in l: 
+7            rank[n] = i
+8            i += 1
+9        result = []
+10        for num in arr:
+11            r = rank[num]
+12            result.append(r)   
+13        return result
