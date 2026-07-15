b2c8583eccaa2358d223f9fb1b431b63083d2d70
Kousttav Biswas <kousttavb@gmail.com>
2026-07-15 21:23:29 +0530

    Add solution for GCD of Odd and Even Sums

--- LeetCode/GCD-of-Odd-and-Even-Sums/Solution.py
+++ LeetCode/GCD-of-Odd-and-Even-Sums/Solution.py
@@ -1,15 +1,4 @@
 1class Solution:
 2    def gcdOfOddEvenSums(self, n: int) -> int:
-3        so=se=0
-4        oc=ec=0
-5        num=0
-6        while oc<n or ec<n:
-7            if num%2==0:
-8                ec+=1
-9                se+=num
-10            else:
-11                oc+=1
-12                so+=num
-13            num+=1
-14        return math.gcd(so,se)
-15        
+3        return n
+4        
