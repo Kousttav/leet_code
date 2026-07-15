16f103139137d544b860d6772a8c64d00513c663
Kousttav Biswas <kousttavb@gmail.com>
2026-07-15 12:15:23 +0530

    Add solution for Range Sum Query - Immutable

--- LeetCode/Range-Sum-Query---Immutable/Solution.py
+++ LeetCode/Range-Sum-Query---Immutable/Solution.py
@@ -1,9 +1,13 @@
-class NumArray:
-
-    def __init__(self, nums: List[int]):
-        self.prefix = [0]
-        for n in nums:
-            self.prefix.append(n + self.prefix[-1])
-
-    def sumRange(self, left: int, right: int) -> int:
-        return self.prefix[right + 1] - self.prefix[left]
+1class NumArray:
+2
+3    def __init__(self, nums: List[int]):
+4        self.prefix = [0]
+5        for n in nums:
+6            self.prefix.append(n + self.prefix[-1])
+7
+8    def sumRange(self, left: int, right: int) -> int:
+9        return self.prefix[right + 1] - self.prefix[left]
+10
+11# Your NumArray object will be instantiated and called as such:
+12# obj = NumArray(nums)
+13# param_1 = obj.sumRange(left,right)
