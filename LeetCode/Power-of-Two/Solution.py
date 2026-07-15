2eaf3cedb948ffed8c4ca81e222979bc877dcd0d
Kousttav Biswas <kousttavb@gmail.com>
2026-07-15 21:56:35 +0530

    Add solution for Power of Two

--- LeetCode/Power-of-Two/Solution.py
+++ LeetCode/Power-of-Two/Solution.py
@@ -1,8 +1,6 @@
 1class Solution:
 2    def isPowerOfTwo(self, n: int) -> bool:
-3        if n <= 0:
+3        if n<=0:
 4            return False
-5        while n % 2 == 0:
-6            n //= 2
-7        return n == 1
-8        
+5        return (n & (n-1))==0
+6        
