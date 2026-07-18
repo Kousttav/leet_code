479d0bda3b030b3086db5f8272d2cae73d4a8d1f
Kousttav Biswas <kousttavb@gmail.com>
2026-07-18 13:29:14 +0530

    Add solution for Three Divisors

--- LeetCode/Three-Divisors/Solution.py
+++ LeetCode/Three-Divisors/Solution.py
@@ -1,7 +1,7 @@
 1class Solution:
 2    def isThree(self, n: int) -> bool:
-3        c=0
-4        for i in range(1,n+1):
+3        c=1
+4        for i in range(1,n//2+1):
 5            if n%i==0:
 6                c+=1
 7        return c==3
