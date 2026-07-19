30bd534c0160d2be700a7d70e256fe0ed0d22ee7
Kousttav Biswas <kousttavb@gmail.com>
2026-07-19 20:57:10 +0530

    Add solution for Reorder Routes to Make All Paths Lead to the City Zero

--- LeetCode/Reorder-Routes-to-Make-All-Paths-Lead-to-the-City-Zero/Solution.py
+++ LeetCode/Reorder-Routes-to-Make-All-Paths-Lead-to-the-City-Zero/Solution.py
@@ -6,19 +6,17 @@
 6            graph[v].append((u,1))
 7            graph[u].append((v,0))
 8        count=0
-9        visited=[False]*(n+1)
-10        def dfs(node,prev):
-11            nonlocal count
-12            for v,c in graph[node]:
-13                if prev == v:
-14                    continue
-15                elif c == 1:
-16                    count+=1
-17                dfs(v,node)
-18        
-19        dfs(0,-1)
-20        return count
-21                
-22
-23
-24        
+9        def dfs(node,prev):
+10            nonlocal count
+11            for v,c in graph[node]:
+12                if prev == v:
+13                    continue
+14                elif c == 1:
+15                    count+=1
+16                dfs(v,node)
+17        dfs(0,-1)
+18        return count
+19                
+20
+21
+22        
