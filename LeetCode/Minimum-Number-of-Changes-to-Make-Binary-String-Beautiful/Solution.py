27b1ee0ff04c85b3151e5a02b5041f3dbe985197
Kousttav Biswas <kousttavb@gmail.com>
2026-09-04 23:10:09 +0530

    Add solution for Minimum Number of Changes to Make Binary String Beautiful

--- LeetCode/Minimum-Number-of-Changes-to-Make-Binary-String-Beautiful/Solution.py
+++ LeetCode/Minimum-Number-of-Changes-to-Make-Binary-String-Beautiful/Solution.py
@@ -1,9 +1,8 @@
 1class Solution:
 2    def minChanges(self, s: str) -> int:
-3        c=0
-4        for i in range(1,len(s),2):
-5            print(s[i-1:i+1])
-6            if s[i-1:i+1].count('1')==1:
-7                c+=1
-8        return c
-9        
+3        answer = 0
+4        for i in range(0, len(s), 2):
+5            if s[i] != s[i + 1]:
+6                answer += 1
+7        return answer
+8        
