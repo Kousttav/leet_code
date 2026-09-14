e32ee4772cc8f21443eab3f52d4eb170cc36978f
Kousttav Biswas <kousttavb@gmail.com>
2026-09-14 19:10:12 +0530

    Add solution for Minimum Capacity Box

--- LeetCode/Minimum-Capacity-Box/Solution.py
+++ LeetCode/Minimum-Capacity-Box/Solution.py
@@ -1,7 +1,7 @@
 1class Solution:
 2    def minimumIndex(self, capacity: list[int], itemSize: int) -> int:
-3        if max(capacity)<itemSize:
-4            return -1
+3        # if max(capacity)<itemSize:
+4        #     return -1
 5        mx=-1
 6        for i,n in enumerate(capacity):
 7            if n<itemSize:
